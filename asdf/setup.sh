19013480dd8091acf4d4bd8a8aa6624bab3576f4
t-i-0414 <gutlo.ol0414@gmail.com>
2022-04-24 18:51:07 +0900

    update asdf scripts

--- asdf/setup.sh
+++ asdf/setup.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-set -euxo pipefail
+set -euo pipefail
 cd "$(dirname "$0")"
+
 
 ln -snfv "$(pwd)/.tool-versions" "$HOME/.tool-versions"
 ln -snfv "$(pwd)/.asdfrc" "$HOME/.asdfrc"
 
-for plugin in terraform awscli python ruby; do
-  if (asdf plugin add "$plugin" >/dev/null 2>&1); then
-    asdf plugin add "$plugin"
-  fi
-done
+if [ -f $1 ]; then
+  while read line
+    do
+      declare plugin=$(echo $line | cut -d ' ' -f 1)
+      set +e
+      asdf plugin add "$plugin"
+      set -e
+  done < $1
+fi
 
 asdf install
