bfb409744dc769b81495379628388daec1b2d542
t-i-0414 <gutlo.ol0414@gmail.com>
2022-11-19 10:34:44 +0900

    update fish config

--- config.fish
+++ config.fish
@@ -1,5 +1,34 @@
 starship init fish | source
 
+# git
+alias g='git'
+alias ga='git add'
+alias gcm='git commit -m'
+alias gs='git switch'
+alias gp='git push'
+alias gpl='git pull'
+alias gst='git stash'
+alias gsl='git stash list'
+alias gsu='git stash -u'
+alias gsa='git stash apply'
+alias gsx='git stash drop'
+
+# docker
+alias d='docker'
+alias di='docker images'
+alias db='docker build'
+alias dbx='docker buildx build --platform linux/amd64'
+alias dr='docker rm'
+alias dri='docker rmi'
+alias dl='docker ps'
+alias dla='docker ps -a'
+
+# terraform
+alias tf='terraform'
+alias tfp='terraform plan'
+alias tfa='terraform apply'
+
+# other
 alias egrep='egrep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias grep='grep --color=auto'
