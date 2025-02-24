b78e2ba72fa9e14cfa19492b91fc16c421f8fef8
t-i-0414 <takuya.iwashiro@takudev.net>
2025-02-24 19:06:36 +0900

    feat: homebrew setup script

--- homebrew/setup.sh
+++ homebrew/setup.sh
@@ -1,28 +1,45 @@
 #!/bin/bash
-cd "$(dirname "$0")"
 
-if !(type "brew" >/dev/null 2>&1); then
-  echo "installing Homebrew ..."
+cd "$(dirname "$0")" || exit 1
+
+if ! command -v brew >/dev/null 2>&1; then
+  echo "Installing Homebrew..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
-sudo softwareupdate --install-rosetta --agree-to-license
+if command -v brew >/dev/null 2>&1; then
+  if [[ "$(uname -m)" == "arm64" ]]; then
+    echo "eval \"\$($(which /opt/homebrew/bin/brew) shellenv)\"" >>~/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+  else
+    echo "eval \"\$($(which /usr/local/bin/brew) shellenv)\"" >>~/.zprofile
+    eval "$(/usr/local/bin/brew shellenv)"
+  fi
+fi
 
-echo "run brew doctor ..."
+if [[ "$(uname -m)" == "arm64" ]]; then
+  echo "Installing Rosetta 2..."
+  sudo softwareupdate --install-rosetta --agree-to-license
+fi
+
+echo "Running 'brew doctor' ..."
 brew doctor
 
-echo "run brew update ..."
+echo "Running 'brew update' ..."
 brew update
 
-echo "run brew upgrade ..."
+echo "Running 'brew upgrade' ..."
 brew upgrade
 
-echo "run brew bundle ..."
-brew bundle
+if [ -f Brewfile ]; then
+  echo "Running 'brew bundle' ..."
+  brew bundle
+else
+  echo "Skipping 'brew bundle' (No Brewfile found)."
+fi
 
-echo "run brew autoremove ..."
+echo "Running 'brew autoremove' ..."
 brew autoremove
 
-echo "run brew cleanup ..."
+echo "Running 'brew cleanup' ..."
 brew cleanup
