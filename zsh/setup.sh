0c051efd36fac5a699fe313943956f858839939f
t-i-0414 <takuya.iwashiro@takudev.net>
2025-03-23 09:26:06 +0900

    feat: update zsh/setup.sh

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -1,5 +1,39 @@
 #!/bin/bash
 
-for dotfile in .hushlogin .zprofile .zshrc; do
-  ln -snfv "$(pwd)/$dotfile" "$HOME/$dotfile"
-done
+set -euo pipefail
+
+THIS_DIR="$(cd "$(dirname "$0")" && pwd)"
+DOTFILES=(".hushlogin" ".zprofile" ".zshrc")
+
+# shellcheck source=/dev/null
+source "${THIS_DIR}/../utils.sh"
+
+check_requirements() {
+  local missing_files=()
+
+  for file in "${DOTFILES[@]}"; do
+    if [ ! -f "$THIS_DIR/$file" ]; then
+      missing_files+=("$file")
+    fi
+  done
+
+  if [ ${#missing_files[@]} -ne 0 ]; then
+    log "Error: Following files not found in $THIS_DIR:"
+    printf '%s\n' "${missing_files[@]}"
+    return 1
+  fi
+}
+
+install_zsh_config() {
+  for file in "${DOTFILES[@]}"; do
+    ln -snfv "$THIS_DIR/$file" "$HOME/$file"
+    log "Linked $file to $HOME"
+  done
+}
+
+main() {
+  check_requirements || exit 1
+  install_zsh_config || exit 1
+}
+
+main
